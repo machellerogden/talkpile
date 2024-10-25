21f690be319093e0e5eed731cdb6e11b7f749d91
Mac Heller-Ogden <mac@machellerogden.com>
2024-10-25 12:50:09 -0500

    fix mkdir

--- tools/mkdir.js
+++ tools/mkdir.js
@@ -4,9 +4,8 @@ import { mkdir as _mkdir } from 'node:fs/promises';
 export const handler = async (session, agent, { dir_path, cwd = process.cwd(), mode = '777', recursive = true }) => {
     let response;
     try {
-        const { dir_path, cwd = process.cwd() } = args;
         const resolved = path.resolve(cwd, dir_path);
-        if (mode) options.mode = parseInt(mode, 8);
+        if (mode) mode = parseInt(mode, 8);
         await _mkdir(resolved, { mode, recursive });
         response = `Directory "${dir_path}" successfully created.`;
     } catch (error) {
